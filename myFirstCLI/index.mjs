c19a306f36e62cdc4bccb7712d33e1efbac672d2
Pierre M <p.mauriello54@gmail.com>
2023-05-05 21:50:21 +0200

    migrated from .then to await using async func

--- myFirstCLI/index.mjs
+++ myFirstCLI/index.mjs
@@ -9,19 +9,17 @@ const countryCode = countryList.getCode(country);
 const url = `https://date.nager.at/api/v2/publicholidays/${year}/${countryCode}`
 
 const spinner = ora('Loading request')
-spinner.start()
 let holidays;
-axios
-    .get(url)
-    .then((response) => {
-        holidays = response.data;
-        spinner.stop()
-        console.log(`Holidays in ${country} for year ${year}:`)
-        holidays.forEach((holiday) => {
-            console.log(`- ${holiday.date}: ${holiday.name}`);
-        });
-    })
 
-    .catch((error) => {
-        console.log(error);
-    })
+async function holidates() {
+    spinner.start()
+    let response = await axios.get(url);
+    spinner.stop();
+    holidays = response.data;
+    console.log(`Holidays in ${country} for year ${year}:`)
+    holidays.forEach((holiday) => {
+        console.log(`- ${holiday.date}: ${holiday.name}`);
+    });
+}
+
+holidates(country, year);
